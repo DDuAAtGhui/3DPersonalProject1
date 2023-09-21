111d8aadac572e91d0a26549da6fd690e042ea8f
DDuAAtGhui <135325796+DDuAAtGhui@users.noreply.github.com>
2023-09-22 00:23:55 +0900

    asdasd

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,24 +7,17 @@ public class Player : Entity
     public bool isBusy { get; private set; }
     #region states
     public PlayerStateMachine stateMachine { get; private set; }
-    public PlayerIdleState idleState { get; private set; }
-    public PlayerWalkState walkState { get; private set; }
-    public PlayerRunState runState { get; private set; }
     protected override void Awake()
     {
         base.Awake();
 
         stateMachine = new PlayerStateMachine();
 
-        idleState = new PlayerIdleState(this, stateMachine, "Idle");
-        walkState = new PlayerWalkState(this, stateMachine, "Walk");
-        runState = new PlayerRunState(this, stateMachine, "Run");
     }
     #endregion
     protected override void Start()
     {
         base.Start();
-        stateMachine.Initialize(idleState);
     }
 
     protected override void Update()
--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -8,29 +8,24 @@ public class PlayerState
 {
     protected PlayerStateMachine stateMachine;
     protected Player player;
-    string AnimationName;
     protected Rigidbody rb;
     protected float X_Input, Y_Input;
 
     protected float StateTimer = 0f;
     protected bool isAnimationFinishTriggerCalled;
 
-    public PlayerState(Player player, PlayerStateMachine stateMachine, string AnimationName)
+    public PlayerState(Player player, PlayerStateMachine stateMachine)
     {
         this.player = player;
         this.stateMachine = stateMachine;
-        this.AnimationName = AnimationName;
         rb = player.rb;
     }
     public virtual void Enter()
     {
-        Debug.Log("Enter State : " + AnimationName);
-        player.anim.SetBool(AnimationName, true);
         isAnimationFinishTriggerCalled = false;
     }
     public virtual void Update()
     {
-        Debug.Log("Update State : " + AnimationName);
 
         StateTimer -= Time.deltaTime;
 
@@ -41,13 +36,10 @@ public class PlayerState
     }
     public virtual void FixedUpdate()
     {
-        Debug.Log("FixedUpdate State : " + AnimationName);
 
     }
     public virtual void Exit()
     {
-        Debug.Log("Exit State : " + AnimationName);
-        player.anim.SetBool(AnimationName, false);
     }
 
     public void AnimationFinishTrigger() => isAnimationFinishTriggerCalled = true;
