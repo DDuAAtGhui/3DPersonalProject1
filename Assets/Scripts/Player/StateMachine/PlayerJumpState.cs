a8c4e31eef73fea0c6b53dc5d83617ec9063949f
DDuAAtGhui <135325796+DDuAAtGhui@users.noreply.github.com>
2023-10-05 00:26:47 +0900

    asd

--- Assets/Scripts/Player/StateMachine/PlayerJumpState.cs
+++ Assets/Scripts/Player/StateMachine/PlayerJumpState.cs
@@ -12,23 +12,25 @@ public class PlayerJumpState : PlayerAirborneState
     public override void Enter()
     {
         base.Enter();
+        anim.SetBool(player.animIDJump, true);
 
         StateTimer = player.jumpTimeout;
+
+        // 루트 높이 * -2f * 중력 = 원하는 높이까지 도달하는 벨로시티값
+        verticalVelocity = Mathf.Sqrt(player.jumpHeight * -2f * player.Gravity);
     }
 
     public override void Update()
     {
         base.Update();
 
-        anim.SetBool(player.animIDJump, true);
-        // 루트 높이 * -2f * 중력 = 원하는 높이까지 도달하는 벨로시티값
-        verticalVelocity = Mathf.Sqrt(player.jumpHeight * -2f * player.Gravity);
 
-        if (!player.isGrounded && verticalVelocity <= 0f)
+        if (!player.isGrounded && verticalVelocity <= 0f && StateTimer <= 0f)
             stateMachine.ChangeState(player.fallingState);
     }
     public override void Exit()
     {
+        anim.SetBool(player.animIDJump, false);
         base.Exit();
     }
 
